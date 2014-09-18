9ce4a54940d9a3500f8ad1ab7fb50ea477078082
Leon Ho <neonaleon.dev@gmail.com>
2014-09-18 22:19:44 +0800

    added site registration

---
+++ app/scripts/controllers/register_controller.js
@@ -0,0 +1,24 @@
+LandingEmber.RegisterController = Ember.ObjectController.extend({
+  email: '',
+  password: '',
+
+  actions: {
+    register: function(){
+      var _this = this;
+      Ember.$.ajax('http://localhost:8080/register', {
+        type: 'POST',
+        dataType: 'json',
+        data: {
+          email: _this.get('email'),
+          password: _this.get('password')
+        }
+      })
+      .done(function(){
+        _this.transitionToRoute('/');
+      })
+      .fail(function(){
+      });
+    }
+  }
+});
+
